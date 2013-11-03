5b679cce5873afa97e925642785133d81a2d6b72
Sduy <rowincho@live.com>
2013-11-03 14:16:51 -0430

    Agregado Username a Users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131102225103) do
+ActiveRecord::Schema.define(:version => 20131103181830) do
 
   create_table "notices", :force => true do |t|
     t.string   "title"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(:version => 20131102225103) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "username"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
